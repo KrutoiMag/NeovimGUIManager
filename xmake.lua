4ddcef8326a27a67c9ed28998763699049d8e142
KrutoiMag <sasha2k62024@gmail.com>
2025-03-02 01:37:52 +0300

    02.03.2025-01:37

--- xmake.lua
+++ xmake.lua
@@ -2,7 +2,7 @@ set_languages("c++23")
 
 target("NeovimGUIManager")
 do
-    add_files("main.cpp","SourceFiles/rlImGui/*.cpp","SourceFiles/imgui/*.cpp", "SourceFiles/imgui/misc/*.cpp")
+    add_files("SourceFiles/NeovimGUIManager/*.cpp","SourceFiles/rlImGui/*.cpp","SourceFiles/imgui/*.cpp", "SourceFiles/imgui/misc/*.cpp")
     add_includedirs("HeaderFiles","HeaderFiles/rlImGui")
     add_links("raylib")
 end
